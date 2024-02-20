ffda267f263e19d5056fd2bcf30cf2caccfdff88
Gustavo Martins do Santos <gustavo@tfcsolutins.tech>
2024-02-20 18:25:07 -0300

    feat: add new route to list the clients that paid the most between a date range and with limit to the list

--- src/domain/admin/admin.router.js
+++ src/domain/admin/admin.router.js
@@ -7,7 +7,8 @@ module.exports = (app) => {
   const profileController = new ProfileController({ profileModel: Profile, contractModel: Contract, jobModel: Job });
 
   /**
-   * 6. GET /admin/best-profession?start=<date>&end=<date> - Returns the profession that earned the most money (sum of jobs paid) for any contactor that worked in the query time range.
+   * 6. GET /admin/best-profession?start=<date>&end=<date> - Returns the profession that earned the most money
+   * (sum of jobs paid) for any contactor that worked in the query time range.
    */
   router.get('/best-profession', async (req, res, next) => {
     try {
@@ -17,6 +18,25 @@ module.exports = (app) => {
         end: req.query.end
       });
       return res.status(200).send(professionsOrderedByEarnings[0]);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  /**
+   * 7. GET /admin/best-clients?start=<date>&end=<date>&limit=<integer>
+   *  returns the clients the paid the most for jobs in the query time period. limit query parameter should be applied,
+   *  default limit is 2.
+   */
+  router.get('/best-clients', async (req, res, next) => {
+    try {
+      const clientsThatPaidTheMost = await profileController.getClientsThatPaidTheMostWithRangeAndLimit({
+        limit: req.query.limit,
+        start: req.query.start,
+        end: req.query.end,
+        raw: true
+      });
+      return res.status(200).send(clientsThatPaidTheMost);
     } catch (error) {
       next(error);
     }
--- src/domain/profile/profile.controller.js
+++ src/domain/profile/profile.controller.js
@@ -1,5 +1,6 @@
 const { fn, col, Op } = require('sequelize');
 const { applyDateRangeValidation } = require('../@utils/common.validator');
+const { PROFILE_TYPE } = require('./profile.enum');
 
 class ProfileController {
   #profileModel;
@@ -35,6 +36,7 @@ class ProfileController {
     try {
       return await this.#profileModel.findAll({
         raw,
+        where: { type: PROFILE_TYPE.CONTRACTOR },
         attributes: ['profession', [fn('SUM', col('Contractor.Jobs.price')), 'earnings']],
         include: [
           {
@@ -65,6 +67,77 @@ class ProfileController {
     } catch (error) {
       console.error(JSON.stringify(error, Object.getOwnPropertyNames(error)));
       throw new Error(`Error getting the profession that earned the most money by time range=${start}-${end}`);
+    }
+  }
+
+  async getClientsThatPaidTheMostWithRangeAndLimit({ start, end, limit = 2, raw = false } = {}) {
+    const validatedParams = applyDateRangeValidation({ start, end });
+    try {
+      // Worked with this as a raw DB query due to not being able to perform this 'limit' directly from Sequelize Model.
+      // Would love to know if there's a better/cleaner solution to this. :)
+      // PS: Left the code down there (ln 101-131) just to keep it for review as well, so that you know where I got stuck.
+      const startDate = `${validatedParams.start} 00:00:00.000 +00:00`;
+      const endDate = `${validatedParams.end} 23:59:59.000 +00:00`;
+      const query = `
+        SELECT 
+            Profile.id,
+            Profile.firstName,
+            Profile.lastName,
+            SUM(Job.price) AS paidTotal
+        FROM 
+            Profiles AS Profile
+        INNER JOIN 
+            Contracts AS Contract ON Profile.id = Contract.ClientId
+        INNER JOIN 
+            Jobs AS Job ON Contract.id = Job.ContractId
+        WHERE 
+            Profile.type = '${PROFILE_TYPE.CLIENT}'
+            AND Job.paid = ${+true}
+            AND Job.paymentDate BETWEEN '${startDate}' AND '${endDate}'
+        GROUP BY 
+            Profile.id
+        ORDER BY 
+            paidTotal DESC
+        LIMIT ${limit};
+      `;
+      // return await this.#profileModel.findAll({
+      //   raw,
+      //   limit,
+      //   where: { type: PROFILE_TYPE.CLIENT },
+      //   attributes: ['id', [fn('SUM', col('Client.Jobs.price')), 'paidTotal']],
+      //   include: [
+      //     {
+      //       as: 'Client',
+      //       model: this.#contractModel,
+      //       attributes: [],
+      //       include: [
+      //         {
+      //           as: 'Jobs',
+      //           model: this.#jobModel,
+      //           attributes: [],
+      //           where: {
+      //             paid: true,
+      //             paymentDate: {
+      //               [Op.between]: [
+      //                 new Date(`${validatedParams.start}T00:00:00Z`),
+      //                 new Date(`${validatedParams.end}T23:59:59Z`)
+      //               ]
+      //             }
+      //           }
+      //         }
+      //       ]
+      //     }
+      //   ],
+      //   group: ['Profile.id'],
+      //   order: [[col('paidTotal'), 'DESC']]
+      // });
+      const [results] = await this.#profileModel.sequelize.query(query);
+      return results;
+    } catch (error) {
+      console.error(JSON.stringify(error, Object.getOwnPropertyNames(error)));
+      throw new Error(
+        `Error getting the client that paid the most money by time range=${start}-${end} and limit=${limit}`
+      );
     }
   }
 }
