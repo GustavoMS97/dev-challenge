c459ed781b002ed3181a9feb4f1e614a61c5f643
Gustavo Martins do Santos <gustavo@tfcsolutins.tech>
2024-02-19 23:31:20 -0300

    feat: update scripts file with code style changes

--- scripts/seedDb.js
+++ scripts/seedDb.js
@@ -10,13 +10,13 @@ async function seed() {
   await Job.sync({ force: true });
   //insert data
   await Promise.all([
-   Profile.create({
+    Profile.create({
       id: 1,
       firstName: 'Harry',
       lastName: 'Potter',
       profession: 'Wizard',
       balance: 1150,
-      type:'client'
+      type: 'client'
     }),
     Profile.create({
       id: 2,
@@ -24,7 +24,7 @@ async function seed() {
       lastName: 'Robot',
       profession: 'Hacker',
       balance: 231.11,
-      type:'client'
+      type: 'client'
     }),
     Profile.create({
       id: 3,
@@ -32,7 +32,7 @@ async function seed() {
       lastName: 'Snow',
       profession: 'Knows nothing',
       balance: 451.3,
-      type:'client'
+      type: 'client'
     }),
     Profile.create({
       id: 4,
@@ -40,7 +40,7 @@ async function seed() {
       lastName: 'Kethcum',
       profession: 'Pokemon master',
       balance: 1.3,
-      type:'client'
+      type: 'client'
     }),
     Profile.create({
       id: 5,
@@ -48,7 +48,7 @@ async function seed() {
       lastName: 'Lenon',
       profession: 'Musician',
       balance: 64,
-      type:'contractor'
+      type: 'contractor'
     }),
     Profile.create({
       id: 6,
@@ -56,7 +56,7 @@ async function seed() {
       lastName: 'Torvalds',
       profession: 'Programmer',
       balance: 1214,
-      type:'contractor'
+      type: 'contractor'
     }),
     Profile.create({
       id: 7,
@@ -64,7 +64,7 @@ async function seed() {
       lastName: 'Turing',
       profession: 'Programmer',
       balance: 22,
-      type:'contractor'
+      type: 'contractor'
     }),
     Profile.create({
       id: 8,
@@ -72,24 +72,24 @@ async function seed() {
       lastName: 'II Elessar Telcontarvalds',
       profession: 'Fighter',
       balance: 314,
-      type:'contractor'
+      type: 'contractor'
     }),
     Contract.create({
-      id:1,
+      id: 1,
       terms: 'bla bla bla',
-     status: 'terminated',
+      status: 'terminated',
       ClientId: 1,
-      ContractorId:5
+      ContractorId: 5
     }),
     Contract.create({
-      id:2,
+      id: 2,
       terms: 'bla bla bla',
       status: 'in_progress',
       ClientId: 1,
       ContractorId: 6
     }),
     Contract.create({
-      id:3,
+      id: 3,
       terms: 'bla bla bla',
       status: 'in_progress',
       ClientId: 2,
@@ -103,35 +103,35 @@ async function seed() {
       ContractorId: 7
     }),
     Contract.create({
-      id:5,
+      id: 5,
       terms: 'bla bla bla',
       status: 'new',
       ClientId: 3,
       ContractorId: 8
     }),
     Contract.create({
-      id:6,
+      id: 6,
       terms: 'bla bla bla',
       status: 'in_progress',
       ClientId: 3,
       ContractorId: 7
     }),
     Contract.create({
-      id:7,
+      id: 7,
       terms: 'bla bla bla',
       status: 'in_progress',
       ClientId: 4,
       ContractorId: 7
     }),
     Contract.create({
-      id:8,
+      id: 8,
       terms: 'bla bla bla',
       status: 'in_progress',
       ClientId: 4,
       ContractorId: 6
     }),
     Contract.create({
-      id:9,
+      id: 9,
       terms: 'bla bla bla',
       status: 'in_progress',
       ClientId: 4,
@@ -140,91 +140,90 @@ async function seed() {
     Job.create({
       description: 'work',
       price: 200,
-      ContractId: 1,
+      ContractId: 1
     }),
     Job.create({
       description: 'work',
       price: 201,
-      ContractId: 2,
+      ContractId: 2
     }),
     Job.create({
       description: 'work',
       price: 202,
-      ContractId: 3,
+      ContractId: 3
     }),
     Job.create({
       description: 'work',
       price: 200,
-      ContractId: 4,
+      ContractId: 4
     }),
     Job.create({
       description: 'work',
       price: 200,
-      ContractId: 7,
+      ContractId: 7
     }),
     Job.create({
       description: 'work',
       price: 2020,
-      paid:true,
-      paymentDate:'2020-08-15T19:11:26.737Z',
-      ContractId: 7,
+      paid: true,
+      paymentDate: '2020-08-15T19:11:26.737Z',
+      ContractId: 7
     }),
     Job.create({
       description: 'work',
       price: 200,
-      paid:true,
-      paymentDate:'2020-08-15T19:11:26.737Z',
-      ContractId: 2,
+      paid: true,
+      paymentDate: '2020-08-15T19:11:26.737Z',
+      ContractId: 2
     }),
     Job.create({
       description: 'work',
       price: 200,
-      paid:true,
-      paymentDate:'2020-08-16T19:11:26.737Z',
-      ContractId: 3,
+      paid: true,
+      paymentDate: '2020-08-16T19:11:26.737Z',
+      ContractId: 3
     }),
     Job.create({
       description: 'work',
       price: 200,
-      paid:true,
-      paymentDate:'2020-08-17T19:11:26.737Z',
-      ContractId: 1,
+      paid: true,
+      paymentDate: '2020-08-17T19:11:26.737Z',
+      ContractId: 1
     }),
     Job.create({
       description: 'work',
       price: 200,
-      paid:true,
-      paymentDate:'2020-08-17T19:11:26.737Z',
-      ContractId: 5,
+      paid: true,
+      paymentDate: '2020-08-17T19:11:26.737Z',
+      ContractId: 5
     }),
     Job.create({
       description: 'work',
       price: 21,
-      paid:true,
-      paymentDate:'2020-08-10T19:11:26.737Z',
-      ContractId: 1,
+      paid: true,
+      paymentDate: '2020-08-10T19:11:26.737Z',
+      ContractId: 1
     }),
     Job.create({
       description: 'work',
       price: 21,
-      paid:true,
-      paymentDate:'2020-08-15T19:11:26.737Z',
-      ContractId: 2,
+      paid: true,
+      paymentDate: '2020-08-15T19:11:26.737Z',
+      ContractId: 2
     }),
     Job.create({
       description: 'work',
       price: 121,
-      paid:true,
-      paymentDate:'2020-08-15T19:11:26.737Z',
-      ContractId: 3,
+      paid: true,
+      paymentDate: '2020-08-15T19:11:26.737Z',
+      ContractId: 3
     }),
     Job.create({
       description: 'work',
       price: 121,
-      paid:true,
-      paymentDate:'2020-08-14T23:11:26.737Z',
-      ContractId: 3,
-    }),
-    
+      paid: true,
+      paymentDate: '2020-08-14T23:11:26.737Z',
+      ContractId: 3
+    })
   ]);
 }
