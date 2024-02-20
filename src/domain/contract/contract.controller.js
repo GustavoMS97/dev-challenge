f952f5df900b772a165487eceb0078a9f6d102e0
Gustavo Martins do Santos <gustavo@tfcsolutins.tech>
2024-02-20 01:56:11 -0300

    feat: update the get contract by id route with comments and new param structure for controller call

--- src/domain/contract/contract.controller.js
+++ src/domain/contract/contract.controller.js
@@ -1,4 +1,5 @@
 const { Op } = require('sequelize');
+const { CONTRACT_STATUS } = require('./contract.enum');
 
 class ContractController {
   #contractModel;
@@ -7,12 +8,24 @@ class ContractController {
     this.#contractModel = contractModel;
   }
 
-  async getByIdAndProfileId(id, profileId) {
+  async getByIdAndProfileId({ id, profileId }) {
     try {
       return await this.#contractModel.findOne({ where: { id, [Op.or]: [{ ClientId: profileId }, { ContractorId: profileId }] } });
     } catch (error) {
       console.error(JSON.stringify(error, Object.getOwnPropertyNames(error)));
       throw new Error(`Error getting contract by id=${id} and profileId=${profileId}`);
+    }
+  }
+
+  async getByProfileId({ profileId, raw = false }) {
+    try {
+      return await this.#contractModel.findAll({
+        where: { status: { [Op.not]: CONTRACT_STATUS.TERMINATED }, [Op.or]: [{ ClientId: profileId }, { ContractorId: profileId }] },
+        raw
+      });
+    } catch (error) {
+      console.error(JSON.stringify(error, Object.getOwnPropertyNames(error)));
+      throw new Error(`Error getting contract by profileId=${profileId}`);
     }
   }
 }
--- src/domain/contract/contract.router.js
+++ src/domain/contract/contract.router.js
@@ -7,10 +7,23 @@ module.exports = (app) => {
   const { Contract } = app.get('models');
   const { getProfile } = app.get('middlewares');
   const contractController = new ContractController(Contract);
+
+  /**
+   * 1 - GET /contracts/:id Returns the contract by id, only if it belongs to the profile calling
+   */
   router.get('/:id', getProfile, async (req, res) => {
-    const contract = await contractController.getByIdAndProfileId(req.params.id, req.profile.id);
+    const contract = await contractController.getByIdAndProfileId({ id: req.params.id, profileId: req.profile.id });
     if (!contract) return res.status(404).end();
     res.json(contract);
+  });
+
+  /**
+   * 2 - GET /contracts Returns a list of contracts belonging to a user (client or contractor), the list should only contain non terminated contracts.
+   */
+  router.get('/', getProfile, async (req, res) => {
+    const contracts = await contractController.getByProfileId({ profileId: req.profile.id, raw: true });
+    if (!contracts) return res.status(404).end();
+    res.json(contracts);
   });
 
   return router;
