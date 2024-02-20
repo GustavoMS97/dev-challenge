f95528f85ecc5d23f6550c11a53e93248821331c
Gustavo Martins do Santos <gustavo@tfcsolutins.tech>
2024-02-20 11:15:49 -0300

    feat: update profile controller to expect parameters as object, and added new method to update balance by profile id

--- src/domain/profile/profile.controller.js
+++ src/domain/profile/profile.controller.js
@@ -1,16 +1,25 @@
 class ProfileController {
   #profileModel;
 
-  constructor(profileModel) {
+  constructor({ profileModel } = {}) {
     this.#profileModel = profileModel;
   }
 
-  async findById(id) {
+  async findById({ id } = {}) {
     try {
       return await this.#profileModel.findOne({ where: { id } });
     } catch (error) {
       console.error(JSON.stringify(error, Object.getOwnPropertyNames(error)));
       throw new Error(`Error getting profile by id=${id}`);
+    }
+  }
+
+  async updateBalanceById({ id, valueToAdd } = {}) {
+    try {
+      return await this.#profileModel.increment({ balance: valueToAdd }, { where: { id } });
+    } catch (error) {
+      console.error(JSON.stringify(error, Object.getOwnPropertyNames(error)));
+      throw new Error(`Error updating profile=${id} balance=+${valueToAdd}`);
     }
   }
 }
