c50d1e1f8581ccfadfec328e92965f094b3c54e6
Gustavo Martins do Santos <gustavo@tfcsolutins.tech>
2024-02-20 11:14:25 -0300

    feat: add new endpoint so the client is able to pay for the job, as well as documentation for the new method and updated docs for older APIs

--- src/domain/job/job.controller.js
+++ src/domain/job/job.controller.js
@@ -9,7 +9,46 @@ class JobController {
     this.#contractModel = contractModel;
   }
 
-  async getJobsByPaidStatusProfileIdAndContractStatus({ paidStatus, profileId, contractStatus, raw = false }) {
+  async findById({ id } = {}) {
+    try {
+      return await this.#jobModel.findOne({ where: { id } });
+    } catch (error) {
+      console.error(JSON.stringify(error, Object.getOwnPropertyNames(error)));
+      throw new Error(`Error getting job by id=${id}`);
+    }
+  }
+
+  async updatePaidById({ id } = {}) {
+    try {
+      return await this.#jobModel.update({ paid: true, paymentDate: new Date() }, { where: { id } });
+    } catch (error) {
+      console.error(JSON.stringify(error, Object.getOwnPropertyNames(error)));
+      throw new Error(`Error updating job=${id} to paid status with payment date=${new Date()}`);
+    }
+  }
+
+  async getJobByIdAndProfileId({ id, profileId, raw = false } = {}) {
+    try {
+      return await this.#jobModel.findOne({
+        raw,
+        where: { id },
+        include: [
+          {
+            model: this.#contractModel,
+            as: 'Contract',
+            required: true,
+            where: { [Op.or]: [{ ClientId: profileId }, { ContractorId: profileId }] }
+            // attributes: ['status']
+          }
+        ]
+      });
+    } catch (error) {
+      console.error(JSON.stringify(error, Object.getOwnPropertyNames(error)));
+      throw new Error(`Error getting jobs by profileId=${profileId} and id=${id}`);
+    }
+  }
+
+  async getJobsByPaidStatusProfileIdAndContractStatus({ paidStatus, profileId, contractStatus, raw = false } = {}) {
     try {
       return await this.#jobModel.findAll({
         raw,
@@ -26,7 +65,7 @@ class JobController {
       });
     } catch (error) {
       console.error(JSON.stringify(error, Object.getOwnPropertyNames(error)));
-      throw new Error(`Error getting jobs by profileId=${profileId} and contract status=${contractStatus}`);
+      throw new Error(`Error getting jobs by profileId=${profileId}, paid status=${paidStatus}, contract status=${contractStatus}`);
     }
   }
 }
--- src/domain/job/job.router.js
+++ src/domain/job/job.router.js
@@ -1,15 +1,19 @@
 const express = require('express');
-const JobController = require('./job.controller');
+
 const { CONTRACT_STATUS } = require('../contract/contract.enum');
+
+const JobController = require('./job.controller');
+const ProfileController = require('../profile/profile.controller');
 
 module.exports = (app) => {
   const router = express.Router();
-  const { Contract, Job } = app.get('models');
+  const { Contract, Job, Profile } = app.get('models');
   const { getProfile } = app.get('middlewares');
   const jobController = new JobController({ jobModel: Job, contractModel: Contract });
+  const profileController = new ProfileController({ profileModel: Profile });
 
   /**
-   * 1 - GET /jobs/unpaid Get all unpaid jobs for a user (either a client or contractor), for active contracts only.
+   * 3. GET /jobs/unpaid Get all unpaid jobs for a user (either a client or contractor), for active contracts only.
    */
   router.get('/unpaid', getProfile, async (req, res) => {
     const jobs = await jobController.getJobsByPaidStatusProfileIdAndContractStatus({
@@ -18,8 +22,36 @@ module.exports = (app) => {
       profileId: req.profile.id,
       raw: true
     });
-    if (!jobs) return res.status(404).end();
     res.json(jobs);
+  });
+
+  /**
+   * 4. POST /jobs/:job_id/pay - Pay for a job, a client can only pay if his balance >= the amount to pay. The amount should be moved from the client's balance to the contractor balance.
+   */
+  router.post('/:job_id/pay', getProfile, async (req, res) => {
+    const job = await jobController.getJobByIdAndProfileId({ id: req.params.job_id, profileId: req.profile.id });
+    if (!job) {
+      return res.status(404).send({ message: 'Job not found' });
+    }
+    if (job.paid) {
+      return res.status(400).send({ message: 'Job is already paid' });
+    }
+    const clientProfile = await profileController.findById({ id: req.profile.id });
+    if (clientProfile.balance < job.price) {
+      return res.status(400).send({ message: `You don't have enough balance to pay for this job` });
+    }
+    const promiseResults = await Promise.allSettled([
+      profileController.updateBalanceById({ id: req.profile.id, valueToAdd: -job.price }),
+      profileController.updateBalanceById({ id: job.Contract.ContractorId, valueToAdd: job.price }),
+      jobController.updatePaidById({ id: req.params.job_id })
+    ]);
+
+    console.info(
+      'Promise results',
+      promiseResults.map((p) => p.status)
+    );
+
+    return res.status(200).send();
   });
 
   return router;
